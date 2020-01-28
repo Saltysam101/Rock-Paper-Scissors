277fe5c644057ef3d905279d48ec776430bda475
Saltysam101 <55891755+Saltysam101@users.noreply.github.com>
2020-01-28 12:13:53 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+const getUserInput = (userChoice) =>{
+userChoice = userChoice.toLowerCase();
+return userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissors'? userChoice : console.log('Error!')
+}
+const getComputerInput = () => {
+    let randomNum = Math.floor(Math.random() * 3)
+    switch (randomNum) {
+        case 0:
+            return 'rock';
+            break;
+            case 1:
+                return 'paper';
+                break;
+                case 2:
+                    return 'scissors';
+                    break;
+            
+    }
+}
+const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice){
+        return 'It is a tie!'
+    }
+    if (userChoice === 'rock' && computerChoice === 'paper'){
+        return 'The computer wins!'
+    }
+    else{
+        return 'You win!'
+    }
+    if(userChoice === 'paper' && computerChoice === 'scissors'){
+        return 'The computer wins!'
+    }
+    else{
+        return 'You win!'
+    }
+    if(userChoice === 'scissors' && computerChoice === 'rock'){
+        return 'The computer wins!'
+    }
+    else{
+        return 'You win!'
+    }
+}
+const playGame = () => {
+    userChoice = getUserInput('rock');
+    computerChoice = getComputerInput();
+    console.log(`You threw: ${userChoice}`);
+    console.log(`The computer threw: ${computerChoice}`)
+    console.log(determineWinner(userChoice, computerChoice));
+}
